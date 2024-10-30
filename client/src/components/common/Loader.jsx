20e65aaae71704c98b1a84baf7ba720af29105f8
Prashant tiwari <prashanttiwari.vns@gmail.com>
2024-10-30 10:19:28 +0530

    react-circular-progress added for loder

--- client/src/components/common/Loader.jsx
+++ client/src/components/common/Loader.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Loader = () => {
+  const value = 0.66;
   return (
-    <div>Loader</div>
-  )
+    <div   className='w-[200px] h-[200px] '>
+      <CircularProgressbar value={value} maxValue={1} text={`${value * 100}%`} />
+    </div>
+  );
 }
+// style={{ width: 200, height: 200, display:"flex",justifyContent:"center",alignItems:"center",margin:"5pc" }}
 
 export default Loader
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import BookCarousel from '../components/layout/BookCarousel'
 import { useQuery } from '@tanstack/react-query'
 import { fetchBooks } from '../services/bookService'
 import { isPending } from '@reduxjs/toolkit'
+import Loader from '../components/common/Loader'
 
 
 const CATEGORIES = [
@@ -14,7 +15,7 @@ const CATEGORIES = [
   { key: 'youngBestSeller', label: 'YOUNG ADULT BestSeller', category: 'YOUNG ADULT' },
 ];
 
-const Loading = () => <p>Loading...</p>;
+
 const Error = ({ message }) => <p>Error: {message}</p>;
 
 const Home = () => {
@@ -28,7 +29,7 @@ const Home = () => {
           queryFn: () => fetchBooks(category, 0, 9)
         });
 
-        if (isPending) return <Loading key={key} />
+        if (isPending) return <div key={key} className='flex justify-center items-center my-5'> <Loader /></div>
         if (isError) return <Error key={key} message={error.message} />
         return data && <BookCarousel key={key} title={label} books={data} />;
       })}
